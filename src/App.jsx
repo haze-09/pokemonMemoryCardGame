214c4b07b8f988b1e2a25f96bb393e59af066b5a
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-09-30 19:55:42 +0530

    background image and logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
 import "./styles/App.css";
-import { fetchPokeData } from "./api";
-
+import pokeMemoryImg from "./assets/Poke-Memory.png";
+import { useState, useEffect } from "react";
 
 function App() {
-  console.log(fetchPokeData(3));
+
+  const openDifficultyModal = () => {
+    document.querySelector("#difficulty").showModal();
+  };
+
+  const closeDifficultyModal = () => {
+    document.querySelector("#difficulty").close();
+  };
+
+  useEffect(() => {
+    openDifficultyModal();
+  }, []);
+
+  const selectDifficulty = (noOfIDs) =>{
+
+
+
+  }
+
+ 
+
   return (
     <>
-      <p>hi</p>
+      <header>
+        <img src={pokeMemoryImg} alt="Poke-Memory" />
+      </header>
+      <dialog id="difficulty">
+        <h2>Choose Difficulty: </h2>
+        <button onClick={closeDifficultyModal}>X</button>
+        <button onClick={() => selectDifficulty(5)}>Easy</button>
+        <button onClick={() => selectDifficulty(10)}>Medium</button>
+        <button onClick={() => selectDifficulty(15)}>Hard</button>
+      </dialog>
     </>
   );
 }
